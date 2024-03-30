a99f9a9d68ea0c44840c5cabfecccfca0b428250
MatteoRigoni <matteo.rigoni2@gmail.com>
2024-03-30 22:23:20 +0100

    .

--- src/app/hour-minute.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HourMinutePipe } from './hour-minute.pipe';
-
-describe('HourMinutePipe', () => {
-  it('create an instance', () => {
-    const pipe = new HourMinutePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
